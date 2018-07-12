0e7ac4e096a8c0b535a3bcdc2d2f15346067a67b
Eric Nguyen <19nguyene@tesdk12.net>
2018-07-12 10:37:26 -0400

    update responsiveness

--- v1/main.js
+++ v1/main.js
@@ -1,5 +1,3 @@
 function changeBackground() {
   document.body.style.backgroundImage = "url('https://www.topbirthdayquotes.com/wp-content/uploads/2017/03/happy-birthday-cake-gif.gif')";
-  document.body.style.backgroundSize = "1280px 610px";
-  document.body.style.backgroundRepeat = "no-repeat";
 }
--- v2/sketch.js
+++ v2/sketch.js
@@ -1,31 +1,31 @@
-const width = 1272;
-const height = 540;
-let msg;
-
 function setup() {
-  createCanvas(width, height);
-  msg = new MSG("HAPPY BIRTHDAY!");
+  createCanvas(windowWidth, windowHeight);
+  msg = new MSG("HAPPY BIRTHDAY");
 }
 
 function draw() {
-  msg.speed = document.getElementById("speed").value;
-  document.getElementById("speedCounter").innerHTML = msg.speed;
+  document.getElementById("speedCounter").innerHTML = document.getElementById("speed").value;
   background(255);
+  drawMSG(msg);
+}
+
+function drawMSG(msg) {
+  msg.speed = document.getElementById("speed").value;
   msg.draw();
   msg.bounce();
   msg.move();
 }
 
 function MSG(msg) {
-  this.x = random(100, width - 200);
-  this.y = random(100, height - 200);
+  this.size = windowWidth / 25;
+  this.x = random(this.size, windowWidth - this.size * 2);
+  this.y = random(this.size, windowHeight - this.size * 2);
   this.speed = 5;
   this.xvel = random([-1, 1]);
   this.yvel = random([-1, 1]);
   this.msg = msg;
-  this.size = 32;
-  this.x_scl = msg.length * this.size * 0.625;
-  this.y_scl = this.size;
+  this.xscl = msg.length * this.size * 0.625;
+  this.yscl = this.size;
   
   this.draw = function() {
     textSize(this.size);
@@ -38,10 +38,14 @@ function MSG(msg) {
   };
   
   this.bounce = function() {
-    if (this.y + this.y_scl > height || this.y < 25) {
-      this.yvel *= -1
+    let lower_bound = this.y + this.yscl > windowHeight && this.yvel > 0;
+    let upper_bound = this.y < 25 && this.yvel < 0;
+    let right_bound = this.x + this.xscl > windowWidth && this.xvel > 0;
+    let left_bound = this.x < 0 && this.xvel < 0;
+    if (lower_bound || upper_bound) {
+      this.yvel *= -1;
     }
-    if (this.x + this.x_scl > width || this.x < 0) {
+    if (right_bound || left_bound) {
       this.xvel *= -1
     }
   };
